8d9a957e18f5f126ee56393ab1ae5e26fd4e5115
Pablom25 <pablomaych@icloud.com>
2025-11-14 02:35:42 +0100

    feat: generator function

---
+++ generator.py
@@ -0,0 +1,22 @@
+from langchain.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_community.chat_models import ChatOllama
+
+def generator(docs:list, question:str) -> str:
+    '''Takes documents and question, returns answer'''
+
+    # Template Prompt
+    template = """Answer the question based only on the following context:
+    {context}
+
+    Question: {question}
+    """
+    prompt = ChatPromptTemplate.from_template(template)
+
+    # LLM
+    llm = ChatOllama(model="llama3.1:8b", temperature=0)
+
+    # Chain
+    chain = prompt | llm | StrOutputParser()
+    answer = chain.invoke({"context": docs, "question": question})
+    return answer
