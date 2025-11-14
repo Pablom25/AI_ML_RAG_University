7c593a956f059b4bf3aeb4bc47e0179745606ea0
Pablom25 <pablomaych@icloud.com>
2025-11-14 02:35:52 +0100

    ref: main function

--- main.py
+++ main.py
@@ -1,30 +1,15 @@
-from langchain_community.vectorstores import Chroma
-from langchain_ollama import OllamaEmbeddings
-from langchain.prompts import ChatPromptTemplate
-from langchain_core.output_parsers import StrOutputParser
-from langchain_community.chat_models import ChatOllama
+from doc_loader import doc_loader
+from retriever import retriever
+from generator import generator
 
-# Question
-question = "What topic does anthony talk about in his essay?"
+def main():
+    question = "What topic does anthony talk about in his essay?"
 
-# Retrieve
-embedding = OllamaEmbeddings(model="mxbai-embed-large")  # same model as build-time
-db = Chroma(persist_directory="chroma", embedding_function=embedding)
-retriever = db.as_retriever(search_kwargs={"k": 3})
-docs = retriever.get_relevant_documents(question)
+    doc_loader("data")
+    docs = retriever(question)
+    answer = generator(docs, question)
 
-# Template Prompt
-template = """Answer the question based only on the following context:
-{context}
+    print(answer)
 
-Question: {question}
-"""
-prompt = ChatPromptTemplate.from_template(template)
-
-# LLM
-llm = ChatOllama(model="llama3.1:8b", temperature=0)
-
-# Chain
-chain = prompt | llm | StrOutputParser()
-answer = chain.invoke({"context": docs, "question": question})
-print(answer)
+if __name__ == "__main__":
+    main()
