6c87cd3c2149ed503f683d051caed7550c46be7a
Pablom25 <pablomaych@icloud.com>
2025-11-14 01:34:55 +0100

    feat: basic full version of code

--- main.py
+++ main.py
@@ -1,7 +1,30 @@
-import ollama
+from langchain_community.vectorstores import Chroma
+from langchain_ollama import OllamaEmbeddings
+from langchain.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_community.chat_models import ChatOllama
 
-response = ollama.generate(model="llama3.1:8b", prompt="Why is the sky blue?", stream=True)
-# Stream response
-for chunk in response:
-    data = chunk["response"]
-    print(data, end="")
+# Question
+question = "What topic does anthony talk about in his essay?"
+
+# Retrieve
+embedding = OllamaEmbeddings(model="mxbai-embed-large")  # same model as build-time
+db = Chroma(persist_directory="chroma", embedding_function=embedding)
+retriever = db.as_retriever(search_kwargs={"k": 3})
+docs = retriever.get_relevant_documents(question)
+
+# Template Prompt
+template = """Answer the question based only on the following context:
+{context}
+
+Question: {question}
+"""
+prompt = ChatPromptTemplate.from_template(template)
+
+# LLM
+llm = ChatOllama(model="llama3.1:8b", temperature=0)
+
+# Chain
+chain = prompt | llm | StrOutputParser()
+answer = chain.invoke({"context": docs, "question": question})
+print(answer)
