1a302608025937b4849e2f6a9a524b2162f03b6d
Pablom25 <pablomaych@icloud.com>
2025-11-14 02:35:21 +0100

    ref: doc_loader function

--- doc_loader.py
+++ doc_loader.py
@@ -3,19 +3,25 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
 from langchain_ollama import OllamaEmbeddings
 
-loader = DirectoryLoader(
-    "data",
-    glob="*.txt",
-    loader_cls=TextLoader,
-    loader_kwargs={"encoding": "utf-8"},
-)
-docs = loader.load()
-print("docs:", len(docs))
+def doc_loader(path:str) -> Chroma:
+    '''Takes data path, loads it, splits it, embeds it, and returns the vector store'''
 
-splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=300)
-chunks = splitter.split_documents(docs)
-print("chunks:", len(chunks))
+    loader = DirectoryLoader(
+        path,
+        glob="*.txt",
+        loader_cls=TextLoader,
+        loader_kwargs={"encoding": "utf-8"},
+    )
+    docs = loader.load()
+    print("docs:", len(docs))
 
-embedding = OllamaEmbeddings(model="mxbai-embed-large")
-db = Chroma.from_documents(chunks, embedding, persist_directory="chroma")
-print("Chroma DB created at ./chroma")
+    splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=300)
+    chunks = splitter.split_documents(docs)
+    print("chunks:", len(chunks))
+
+    embedding = OllamaEmbeddings(model="mxbai-embed-large")
+    db = Chroma.from_documents(chunks, embedding, persist_directory="chroma")
+    return db
+
+if __name__ == "__main__":
+    doc_loader("data")
