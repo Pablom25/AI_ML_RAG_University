bd2ddbb05aedb4b29e9990a5e8b30025053d4ca7
Pablom25 <pablomaych@icloud.com>
2025-11-15 20:20:04 +0100

    fix: add new features to app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 from doc_loader import doc_loader
+from routing import detect_route, get_known_students
 from retriever import retriever
 from generator import generator
-
 
 def main():
     st.title("AI Admissions Helper")
@@ -13,11 +13,17 @@ def main():
         if question.strip() == "":
             st.warning("Please enter a question.")
         else:
-            # Build / update the Chroma DB (same as in main.py)
+            # Build / update the Chroma DB with metadata
             doc_loader("data")
 
-            # Retrieve docs and generate answer
-            docs = retriever(question)
+            # Route to relevant student and/or university data
+            known_students = get_known_students()
+            route = detect_route(question, known_students)
+
+            # Retrieve relevant docs
+            docs = retriever(question, route.student, route.intent, known_students)
+
+            # Generate answer
             answer = generator(docs, question)
 
             st.text_area("Answer:", value=answer, height=200)
