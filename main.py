8f310fc51e3fb389a6fbfcc0f88820be60afad80
Pablom25 <pablomaych@icloud.com>
2025-11-15 18:46:26 +0100

    fix: adjust main to routing changes

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 from doc_loader import doc_loader
+from routing import detect_route, get_known_students
 from retriever import retriever
 from generator import generator
 
 def main():
-    question = "What topic does anthony talk about in his essay?"
+    question = "Does Sofia Martinez's admission essay relate to the university values?"
 
     doc_loader("data")
-    docs = retriever(question)
+    known_students = get_known_students()
+    route = detect_route(question, known_students)
+    print("Route: ", route)
+    docs = retriever(question, route.student, route.intent, known_students)
+    print("Docs: ", docs)
     answer = generator(docs, question)
-
-    print(answer)
+    print("\n Answer: \n", answer)
 
 if __name__ == "__main__":
     main()
